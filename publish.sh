8f1df3c5aadda26c285c7ab1129b6d2d179542df
tanzr <tannaznr@mail.uio.no>
2019-01-17 10:26:29 +0100

    Added new style

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eux
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+#echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
 mkdir content
@@ -8,14 +8,14 @@ hugo
 rmdir content
 
 # Add changes to git.
-git add -A public/
+#git add -A public/
 
 # Commit changes.
-msg="publish.sh: update github pages"
-git commit -m "$msg"
+#msg="publish.sh: update github pages"
+#git commit -m "$msg"
 
 # Push to master
-git push origin master
+#git push origin master
 
 # Do a subtree push to the public_html branch
-git subtree push --prefix=public git@github.com:norcams/www_docs.git gh-pages
+#git subtree push --prefix=public git@github.com:norcams/www_docs.git gh-pages
